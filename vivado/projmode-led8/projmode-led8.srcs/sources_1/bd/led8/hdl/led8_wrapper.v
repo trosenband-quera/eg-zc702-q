bb5c51e07cbb067220af9ed1bd3d9bf63f56a536
zynq developer <aluminizer@zynqdev.local>
2025-11-05 14:01:30 -0500

    C++ ok

--- vivado/projmode-led8/projmode-led8.srcs/sources_1/bd/led8/hdl/led8_wrapper.v
+++ vivado/projmode-led8/projmode-led8.srcs/sources_1/bd/led8/hdl/led8_wrapper.v
@@ -1,3 +1,12 @@
+//Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
+//Date        : Wed Nov  5 13:10:04 2025
+//Host        : zynqdev running 64-bit Ubuntu 16.04.7 LTS
+//Command     : generate_target led8_wrapper.bd
+//Design      : led8_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
 `timescale 1 ps / 1 ps
 
 module led8_wrapper
