ea63487dfc63d82df326132d0c06057af50c2f79
zynq developer <aluminizer@zynqdev.local>
2025-11-06 15:54:08 -0500

    more vivado

--- vivado/project2/recreate.tcl
+++ vivado/project2/recreate.tcl
@@ -101,7 +101,7 @@ if { $::argc > 0 } {
 set orig_proj_dir "[file normalize "$origin_dir/"]"
 
 # Create project
-create_project ${_xil_proj_name_} ./${_xil_proj_name_} -part xc7z020clg484-1
+create_project ${_xil_proj_name_} ./${_xil_proj_name_} -part xc7z020clg484-1 -force
 
 # Set the directory path for the new project
 set proj_dir [get_property directory [current_project]]
