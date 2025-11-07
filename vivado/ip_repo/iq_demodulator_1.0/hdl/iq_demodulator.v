ca1f125748c6d9675e5023488d7ef7b4166b5e03
Till Rosenband <trosenband@quera.com>
2025-11-07 09:12:48 -0500

    make number of channels parameterizable

--- vivado/ip_repo/iq_demodulator_1.0/hdl/iq_demodulator.v
+++ vivado/ip_repo/iq_demodulator_1.0/hdl/iq_demodulator.v
@@ -1,40 +1,31 @@
 module iq_demodulator #(
-    parameter integer FREQ0 = 100000,
-    parameter integer FREQ1 = 110000,
-    parameter integer FREQ2 = 120000,
+    parameter integer NUM_CHANNELS = 3,
     parameter integer PHASE_WIDTH = 32,
-    parameter integer LUT_SIZE = 256
+    parameter integer LUT_SIZE = 256,
+    parameter integer freq [0:NUM_CHANNELS-1] = '{100000, 110000, 120000}
 ) (
     input wire clk,                  // 100 MHz system clock
     input wire rst,
     output reg [15:0] adc_data_reg,
     output reg [31:0] adc_sample_count,
-    output wire [15:0] phase0,
-    output wire [15:0] phase1,
-    output wire [15:0] phase2
+    output wire [15:0] phase[NUM_CHANNELS-1:0]
 );
 
 // DDS parameters
 localparam COS_OFFSET = LUT_SIZE / 4;
 localparam SAMPLE_RATE = 1000000;
 
-// Phase increment = (freq * LUT_SIZE) / SAMPLE_RATE
-localparam DDS_PHASE_INC0 = (FREQ0 * LUT_SIZE) / SAMPLE_RATE;
-localparam DDS_PHASE_INC1 = (FREQ1 * LUT_SIZE) / SAMPLE_RATE;
-localparam DDS_PHASE_INC2 = (FREQ2 * LUT_SIZE) / SAMPLE_RATE;
-
-// DDS phase accumulators
-reg [PHASE_WIDTH-1:0] dds_phase_acc0 = 0;
-reg [PHASE_WIDTH-1:0] dds_phase_acc1 = 0;
-reg [PHASE_WIDTH-1:0] dds_phase_acc2 = 0;
-
+// Phase increment array
+localparam int DDS_PHASE_INC[NUM_CHANNELS];
+integer ch;
+initial begin
+    for (ch = 0; ch < NUM_CHANNELS; ch = ch + 1) begin
+        DDS_PHASE_INC[i] = (freq[i] * LUT_SIZE) / SAMPLE_RATE;
+    end
+end
 // DDS phase accumulators (separate for sin and cos)
-reg [PHASE_WIDTH-1:0] dds_phase_acc0_sin = 0;
-reg [PHASE_WIDTH-1:0] dds_phase_acc0_cos = 0;
-reg [PHASE_WIDTH-1:0] dds_phase_acc1_sin = 0;
-reg [PHASE_WIDTH-1:0] dds_phase_acc1_cos = 0;
-reg [PHASE_WIDTH-1:0] dds_phase_acc2_sin = 0;
-reg [PHASE_WIDTH-1:0] dds_phase_acc2_cos = 0;
+reg [PHASE_WIDTH-1:0] dds_phase_acc_sin[NUM_CHANNELS-1:0];
+reg [PHASE_WIDTH-1:0] dds_phase_acc_cos[NUM_CHANNELS-1:0];
 
 // LUT for sine only
 reg signed [15:0] sin_lut [0:LUT_SIZE-1];
@@ -48,96 +39,73 @@ initial begin
 end
 
 // DDS update (separate for sin and cos)
-wire [7:0] addr0_sin = dds_phase_acc0_sin[PHASE_WIDTH-1 -: 8];
-wire [7:0] addr0_cos = dds_phase_acc0_cos[PHASE_WIDTH-1 -: 8];
-wire [7:0] addr1_sin = dds_phase_acc1_sin[PHASE_WIDTH-1 -: 8];
-wire [7:0] addr1_cos = dds_phase_acc1_cos[PHASE_WIDTH-1 -: 8];
-wire [7:0] addr2_sin = dds_phase_acc2_sin[PHASE_WIDTH-1 -: 8];
-wire [7:0] addr2_cos = dds_phase_acc2_cos[PHASE_WIDTH-1 -: 8];
+wire [7:0] addr_sin[NUM_CHANNELS-1:0];
+wire [7:0] addr_cos[NUM_CHANNELS-1:0];
+genvar ch;
+generate
+    for (ch = 0; ch < NUM_CHANNELS; ch = ch + 1) begin : dds_addr_gen
+        assign addr_sin[ch] = dds_phase_acc_sin[ch][PHASE_WIDTH-1 -: 8];
+        assign addr_cos[ch] = dds_phase_acc_cos[ch][PHASE_WIDTH-1 -: 8];
+    end
+endgenerate
 
 // Mixers (use same LUT for sin and cos)
 wire signed [15:0] in_signal = adc_data_reg;
-wire signed [31:0] i0 = in_signal * sin_lut[addr0_cos];
-wire signed [31:0] q0 = in_signal * sin_lut[addr0_sin];
-wire signed [31:0] i1 = in_signal * sin_lut[addr1_cos];
-wire signed [31:0] q1 = in_signal * sin_lut[addr1_sin];
-wire signed [31:0] i2 = in_signal * sin_lut[addr2_cos];
-wire signed [31:0] q2 = in_signal * sin_lut[addr2_sin];
+wire signed [31:0] i[NUM_CHANNELS-1:0];
+wire signed [31:0] q[NUM_CHANNELS-1:0];
+generate
+    for (ch = 0; ch < NUM_CHANNELS; ch = ch + 1) begin : mixer_gen
+        assign i[ch] = in_signal * sin_lut[addr_cos[ch]];
+        assign q[ch] = in_signal * sin_lut[addr_sin[ch]];
+    end
+endgenerate
 
 // Simple moving average filter (low-pass)
-reg signed [31:0] i_avg0 = 0, q_avg0 = 0;
-reg signed [31:0] i_avg1 = 0, q_avg1 = 0;
-reg signed [31:0] i_avg2 = 0, q_avg2 = 0;
-
+reg signed [31:0] i_avg[NUM_CHANNELS-1:0];
+reg signed [31:0] q_avg[NUM_CHANNELS-1:0];
 
 // ADC data sampling and counting, on xadc_ready rising edge
 reg prev_xadc_ready;
 
+integer ch_idx;
 always @(posedge clk) begin
     if (rst) begin
         adc_sample_count <= 0;
         adc_data_reg <= 0;
         prev_xadc_ready <= 0;
-
-        dds_phase_acc0_sin <= 0;
-        dds_phase_acc0_cos <= COS_OFFSET;
-        dds_phase_acc1_sin <= 0;
-        dds_phase_acc1_cos <= COS_OFFSET;
-        dds_phase_acc2_sin <= 0;
-        dds_phase_acc2_cos <= COS_OFFSET;
-
+        for (ch_idx = 0; ch_idx < NUM_CHANNELS; ch_idx = ch_idx + 1) begin
+            dds_phase_acc_sin[ch_idx] <= 0;
+            dds_phase_acc_cos[ch_idx] <= COS_OFFSET;
+            i_avg[ch_idx] <= 0;
+            q_avg[ch_idx] <= 0;
+        end
     end else begin
         if (xadc_ready && !prev_xadc_ready) begin
             adc_sample_count <= adc_sample_count + 1;
             adc_data_reg <= adc_data;
-
-            // Update DDS phase accumulators (separate for sin and cos)
-            dds_phase_acc0_sin <= dds_phase_acc0_sin + DDS_PHASE_INC0;
-            dds_phase_acc0_cos <= dds_phase_acc0_cos + DDS_PHASE_INC0;
-            dds_phase_acc1_sin <= dds_phase_acc1_sin + DDS_PHASE_INC1;
-            dds_phase_acc1_cos <= dds_phase_acc1_cos + DDS_PHASE_INC1;
-            dds_phase_acc2_sin <= dds_phase_acc2_sin + DDS_PHASE_INC2;
-            dds_phase_acc2_cos <= dds_phase_acc2_cos + DDS_PHASE_INC2;
-
-            // Update moving average filters
-            i_avg0 <= (i_avg0 >> 1) + (i0 >> 1);
-            q_avg0 <= (q_avg0 >> 1) + (q0 >> 1);
-
-            i_avg1 <= (i_avg1 >> 1) + (i1 >> 1);
-            q_avg1 <= (q_avg1 >> 1) + (q1 >> 1);
-
-            i_avg2 <= (i_avg2 >> 1) + (i2 >> 1);
-            q_avg2 <= (q_avg2 >> 1) + (q2 >> 1);
+            for (ch_idx = 0; ch_idx < NUM_CHANNELS; ch_idx = ch_idx + 1) begin
+                dds_phase_acc_sin[ch_idx] <= dds_phase_acc_sin[ch_idx] + DDS_PHASE_INC[ch_idx];
+                dds_phase_acc_cos[ch_idx] <= dds_phase_acc_cos[ch_idx] + DDS_PHASE_INC[ch_idx];
+                i_avg[ch_idx] <= (i_avg[ch_idx] >> 1) + (i[ch_idx] >> 1);
+                q_avg[ch_idx] <= (q_avg[ch_idx] >> 1) + (q[ch_idx] >> 1);
+            end
         end
         prev_xadc_ready <= xadc_ready;
     end
 end
 
-// Phase calculation using CORDIC 
-cordic_atan2 cordic0 (
-    .clk(clk),
-    .rst(rst),
-    .x(i_avg0[31:16]),
-    .y(q_avg0[31:16]),
-    .phase(phase0)
-);
-
-cordic_atan2 cordic1 (
-    .clk(clk),
-    .rst(rst),
-    .x(i_avg1[31:16]),
-    .y(q_avg1[31:16]),
-    .phase(phase1)
-);
-
-cordic_atan2 cordic2 (
-    .clk(clk),
-    .rst(rst),
-    .x(i_avg2[31:16]),
-    .y(q_avg2[31:16]),
-    .phase(phase2)
-);
-
+// Phase calculation using CORDIC
+generate
+    for (ch = 0; ch < NUM_CHANNELS; ch = ch + 1) begin : cordic_gen
+        cordic_atan2 cordic_inst (
+            .clk(clk),
+            .rst(rst),
+            .x(i_avg[ch][31:16]),
+            .y(q_avg[ch][31:16]),
+            .phase(phase[ch])
+        );
+    end
+endgenerate
 // XADC
 wire xadc_ready;
 
